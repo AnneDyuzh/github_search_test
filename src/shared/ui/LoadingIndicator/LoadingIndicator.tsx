ba0dfd221cb217feb09276a4cc134b3160f61406
AnneDyuzh <anyutka.dyuzheva@mail.ru>
2025-01-20 12:35:29 +0300

    Added commit

--- src/shared/ui/LoadingIndicator/LoadingIndicator.tsx
+++ src/shared/ui/LoadingIndicator/LoadingIndicator.tsx
@@ -2,14 +2,16 @@ import './loadingIndicator.css';
 
 export const LoadingIndicator = () => {
     return (
-        <div className="loading-indicator">
+        {/* Пустые div используются для создания анимации кольца. 
+          div - четверть кольца */}
+        < div className = "loading-indicator" >
             <div className="lds-ring">
                 <div></div>
                 <div></div>
                 <div></div>
                 <div></div>
             </div>
-        </div>
+        </div >
     );
 };
 
